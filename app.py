266fef0bf7f2f505d990732d065bbb9f1cfa6f05
paulmunyao <paulmunyao094@gmail.com>
2022-08-25 14:34:56 +0300

    initialize flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from flask import Flask
 
 import africastalking
 
@@ -16,23 +17,23 @@ class SMS:
         self.sms = africastalking.SMS
 
     def send(self):
-            # Set the numbers you want to send to in international format
-            recipients = ["+254715494857", "+254732841879"]
+        # Set the numbers you want to send to in international format
+        recipients = ["+254715494857", "+254732841879"]
 
-            # Set your message
-            message = "I'm a lumberjack and it's ok, I sleep all night and I work all day";
+        # Set your message
+        message = "I'm a lumberjack and it's ok, I sleep all night and I work all day"
 
-            # Set your shortCode or senderId
-            # sender = "E-daktari"
-            try:
-				# Thats it, hit send and we'll take care of the rest.
-                response = self.sms.send(message, recipients)
-                print (response)
-            except Exception as e:
-                print ('Encountered an error while sending: %s' % str(e))
+        # Set your shortCode or senderId
+        # sender = "E-daktari"
+        try:
+            # Thats it, hit send and we'll take care of the rest.
+            response = self.sms.send(message, recipients)
+            print(response)
+        except Exception as e:
+            print('Encountered an error while sending: %s' % str(e))
+
 
 if __name__ == '__main__':
     SMS().send()
 
-from flask import Flask
-app = Flask(__name__)   
+app = Flask(__name__)
